3f17155fd8cc7e9a82b995dc38d3a31a84eb7b23
Devin Alvaro <devin.alvaro@gmail.com>
2018-07-30 22:28:04 +0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_151340) do
+ActiveRecord::Schema.define(version: 2018_07_30_152605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2018_07_30_151340) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "menus", "restaurants"
 end
