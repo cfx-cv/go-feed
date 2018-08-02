f422e309e4645279e9463e2ea3beffd859e3a32d
NickyLogan <laurentiusdominick@yahoo.com>
2018-08-03 03:07:25 +0700

    Add database seed for sample data
    Run rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([
+  {
+    name: "Restoran Nasi",
+    address: "Alamat Restoran Nasi"
+  }, {
+    name: "Restoran Mie",
+    address: "Alamat Restoran Mie"
+  }, {
+    name: "Restoran Roti",
+    address: "Alamat Restoran Roti"
+  }
+])
+
+restaurants.each do |restaurant|
+  3.times do |i|
+    Menu.create(
+      name: restaurant.name.split[1] + " " + (65 + i).chr,
+      price: i*1000,
+      restaurant: restaurant
+    )
+  end
+end
