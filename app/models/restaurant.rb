26caf1dedefb517e5c4bb1faed91b2b84a3310f6
Devin Alvaro <devin.alvaro@gmail.com>
2018-08-14 17:37:12 +0000

    Display map from nami

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,16 @@
+require "http"
+require "json"
+
 class Restaurant < ApplicationRecord
   validates :name, :address, presence: true
 
   has_many :menus
   belongs_to :position
+
+  def fetch_map
+    position = self.position
+    resp = HTTP.get("http://suez_app/staticmap?origin=#{position.latitude},#{position.longitude}")
+    json = JSON.parse(resp.to_s)
+    json["staticmap"]
+  end
 end
