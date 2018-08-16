c01deea2625f0b7edadea20bcecd65cb55bc326a
Devin Alvaro <devin.alvaro@gmail.com>
2018-08-16 10:05:29 +0700

    Put user routes outside /app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show, :edit, :create, :update, :destroy]
+  get "/register", to: "users#new"
+
+  resources :user_sessions, only: [:create]
+  get "/login", to: "user_sessions#new"
+  delete "/logout", to: "user_sessions#destroy"
+
   # get "/app", to: "app#index"
 
   scope "/app" do
@@ -9,12 +16,5 @@ Rails.application.routes.draw do
     resources :orders, only: [:index, :show, :create, :update, :destroy]
 
     resources :menus, :drivers
-
-    resources :user_sessions, only: [:create]
-    get "/login", to: "user_sessions#new"
-    delete "/logout", to: "user_sessions#destroy"
-
-    resources :users, only: [:index, :show, :edit, :create, :update, :destroy]
-    get "/register", to: "users#new"
   end
 end
