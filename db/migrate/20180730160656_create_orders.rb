82649b20d079e24dc9d1f37b57092494ad3b7a57
Devin Alvaro <devin.alvaro@gmail.com>
2018-08-16 09:59:12 +0700

    Set Customer/Driver as reference to User

--- db/migrate/20180730160656_create_orders.rb
+++ db/migrate/20180730160656_create_orders.rb
@@ -2,8 +2,8 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
       t.references :restaurant, foreign_key: true
-      t.string :customer_id
-      t.string :driver_id
+      t.references :customer, foreign_key: { to_table: :user }
+      t.references :driver, foreign_key: { to_table: :user }
 
       t.timestamps
     end
