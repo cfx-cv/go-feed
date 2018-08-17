bed34186f968a492175da3de080880d15a1064c1
Devin Alvaro <devin.alvaro@gmail.com>
2018-08-17 21:28:58 +0700

    Remove boilerplates

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -1,9 +1,6 @@
 require "rails_helper"
 
 RSpec.describe RestaurantsController, type: :controller do
-  # This should return the minimal set of attributes required to create a valid
-  # Restaurant. As you add validations to Restaurant, be sure to
-  # adjust the attributes here as well.
   let(:valid_attributes) {
     skip("Add a hash of attributes valid for your model")
   }
@@ -12,9 +9,6 @@ RSpec.describe RestaurantsController, type: :controller do
     skip("Add a hash of attributes invalid for your model")
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # RestaurantsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
   describe "GET #index" do
